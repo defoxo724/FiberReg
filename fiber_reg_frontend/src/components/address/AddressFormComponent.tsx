b2b8267fa55a4435307ed61c9fb75f54e7de35be
defoxo724 <defoxo724@proton.me>
2026-05-14 02:00:47 +0200

    Refactor address form

--- fiber_reg_frontend/src/components/address/AddressFormComponent.tsx
+++ fiber_reg_frontend/src/components/address/AddressFormComponent.tsx
@@ -24,6 +24,74 @@ const AddressFormComponent = (props: AddressFormComponentProps) => {
   const { data: communesData } = useFetchCommunes(addressCreateRequest.province, addressCreateRequest.district);
   const { data: citiesData } = useFetchCities(addressCreateRequest.province, addressCreateRequest.district, addressCreateRequest.commune);
 
+  /*
+  Select
+  */
+  const updateProvince = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAddressCreateRequest({
+      ...addressCreateRequest,
+      province: e.target.value,
+      district: "",
+      commune: "",
+      city: "",
+    });
+  };
+
+  const updateDistrict = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAddressCreateRequest({
+      ...addressCreateRequest,
+      district: e.target.value,
+      commune: "",
+      city: "",
+    });
+  };
+
+  const updateCommune = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAddressCreateRequest({
+      ...addressCreateRequest,
+      commune: e.target.value,
+      city: "",
+    });
+  };
+
+  const updateCity = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAddressCreateRequest({
+      ...addressCreateRequest,
+      city: e.target.value,
+    });
+  };
+
+  /*
+    Inputs
+  */
+  const updateStreet = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddressCreateRequest({
+      ...addressCreateRequest,
+      street: e.target.value,
+    });
+  };
+
+  const updateHouseNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddressCreateRequest({
+      ...addressCreateRequest,
+      houseNumber: parseInt(e.target.value),
+    });
+  };
+
+  const updateApartmentNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddressCreateRequest({
+      ...addressCreateRequest,
+      apartmentNumber: parseInt(e.target.value),
+    });
+  };
+
+  const updateZipCode = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddressCreateRequest({
+      ...addressCreateRequest,
+      zipCode: e.target.value,
+    });
+  };
+
   const handleSubmit = () => {
     if (!addressCreateRequest.province || !addressCreateRequest.district || !addressCreateRequest.commune || !addressCreateRequest.city) return;
 
@@ -36,123 +104,29 @@ const AddressFormComponent = (props: AddressFormComponentProps) => {
   return (
     <div>
       <div>
-        <Select
-          title={"Województwo"}
-          onChange={function (e: React.ChangeEvent<HTMLSelectElement>): void {
-            setAddressCreateRequest({
-              ...addressCreateRequest,
-              province: e.target.value,
-              district: "",
-              commune: "",
-              city: "",
-            });
-          }}
-          options={provincesData?.map((p) => [p, p]) || []}
-          selected={addressCreateRequest.province}
-        />
-
-        <Select
-          title={"Powiat"}
-          onChange={function (e: React.ChangeEvent<HTMLSelectElement>): void {
-            setAddressCreateRequest({
-              ...addressCreateRequest,
-              district: e.target.value,
-              commune: "",
-              city: "",
-            });
-          }}
-          options={districtsData?.map((p) => [p, p]) || []}
-          selected={addressCreateRequest.district}
-        />
-
-        <Select
-          title={"Gmina"}
-          onChange={function (e: React.ChangeEvent<HTMLSelectElement>): void {
-            setAddressCreateRequest({
-              ...addressCreateRequest,
-              commune: e.target.value,
-              city: "",
-            });
-          }}
-          options={communesData?.map((p) => [p, p]) || []}
-          selected={addressCreateRequest.commune}
-        />
-
-        <Select
-          title={"Miasto"}
-          onChange={function (e: React.ChangeEvent<HTMLSelectElement>): void {
-            setAddressCreateRequest({
-              ...addressCreateRequest,
-              city: e.target.value,
-            });
-          }}
-          options={citiesData?.map((p) => [p, p]) || []}
-          selected={addressCreateRequest.city}
-        />
+        <Select title={"Województwo"} onChange={updateProvince} options={provincesData?.map((p) => [p, p]) || []} selected={addressCreateRequest.province} />
+        <Select title={"Powiat"} onChange={updateDistrict} options={districtsData?.map((p) => [p, p]) || []} selected={addressCreateRequest.district} />
+        <Select title={"Gmina"} onChange={updateCommune} options={communesData?.map((p) => [p, p]) || []} selected={addressCreateRequest.commune} />
+        <Select title={"Miasto"} onChange={updateCity} options={citiesData?.map((p) => [p, p]) || []} selected={addressCreateRequest.city} />
       </div>
 
       <div>
         <label htmlFor="street">Ulica</label>
-
-        <input
-          type="text"
-          id="street"
-          value={addressCreateRequest.street}
-          onChange={(e) => {
-            setAddressCreateRequest({
-              ...addressCreateRequest,
-              street: e.target.value,
-            });
-          }}
-        />
+        <input type="text" id="street" value={addressCreateRequest.street} onChange={updateStreet} />
       </div>
-
       <div>
         <label htmlFor="houseNumber">Numer domu</label>
-
-        <input
-          type="text"
-          id="houseNumber"
-          value={addressCreateRequest.houseNumber}
-          onChange={(e) => {
-            setAddressCreateRequest({
-              ...addressCreateRequest,
-              houseNumber: Number(e.target.value),
-            });
-          }}
-        />
+        <input type="text" id="houseNumber" value={addressCreateRequest.houseNumber} onChange={updateHouseNumber} />
       </div>
 
       <div>
         <label htmlFor="apartmentNumber">Numer mieszkania</label>
-
-        <input
-          type="text"
-          id="apartmentNumber"
-          value={addressCreateRequest.apartmentNumber}
-          onChange={(e) => {
-            setAddressCreateRequest({
-              ...addressCreateRequest,
-              apartmentNumber: Number(e.target.value),
-            });
-          }}
-        />
+        <input type="text" id="apartmentNumber" value={addressCreateRequest.apartmentNumber} onChange={updateApartmentNumber} />
       </div>
 
       <div>
         <label htmlFor="zipCode">Kod pocztowy</label>
-
-        <input
-          type="text"
-          id="zipCode"
-          value={addressCreateRequest.zipCode}
-          onChange={(e) => {
-            setAddressCreateRequest({
-              ...addressCreateRequest,
-              zipCode: e.target.value,
-            });
-          }}
-        />
+        <input type="text" id="zipCode" value={addressCreateRequest.zipCode} onChange={updateZipCode} />
       </div>
 
       <div>
